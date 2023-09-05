b1b9faced6931ca05d47988878cd28bfb85e2189
system64user <144166914+system64user@users.noreply.github.com>
2023-09-05 13:22:53 -0700

    Add files via upload

---
+++ 139_files/1_3.js
@@ -0,0 +1,161 @@
+var UnifiedPositionContent={
+  "web_116": [
+    {
+      "contentId": 60395,
+      "showrule": 9,
+      "positionid": 6926,
+      "content": "img=/content/images/20230818/1692322230146.jpg|color=#110f27|key=mainBgHow2UsePoints",
+      "hreflink": "",
+      "expand": "9",
+      "weight": 100
+    }
+  ],
+  "PE_SDK_01": [
+    {
+      "contentId": 59676,
+      "showrule": 2,
+      "positionid": 32335,
+      "content": "{	\"title\": \"\",	\"adImg\": \"\",	\"url\": \"\",	\"display_advert\": 1,	\"action\": 8000,	\"thingId\": \"55052\",	\"authType\": 1,	\"adType\": 2,	\"classname\": \"\",	\"adOption\": 0,	\"place_postion\": 7,	\"sourceFlag\": \"1\",	\"source\": \"139邮箱\",	\"sourceTips\": \"\"}",
+      "expand": "9",
+      "weight": 100
+    }
+  ],
+  "web_421": [
+    {
+      "contentId": 60093,
+      "showrule": 1,
+      "positionid": 32534,
+      "content": "<article class=\"layer_mask\" id=\"verificationBoxMask\" style=\"z-index:9998;\"></article><div class=\"tipsBox tipsBoxtThree\" id=\"verificationBox\" style=\"position:fixed;z-index:9999;border:none;right:8px;left:8px;bottom:1px;padding:0;height:127px;background:transparent;box-shadow:none;\"><img id=\"closeVerificationBoxBtn\" src=\"/images/close_ad.png\" style=\"position:absolute;right:10px;top:16px;width:20px;\" alt=\"\" /><img id=\"toDownload\" relurl=\"https://happy.mail.10086.cn/jsp/cn/gift/login/sso.do?sourceid=3006\" src=\"https://img.zone139.com/web_content_op/content/images/20230726/1690366489433.png\" style=\"width:100%;height:100%;object-fit:cover;\" alt=\"\" /> </div>",
+      "hreflink": "",
+      "expand": "9",
+      "weight": 100
+    }
+  ],
+  "web_430": [
+    {
+      "contentId": 60145,
+      "showrule": 0,
+      "positionid": 32633,
+      "content": "<div id=\"mailListToApp\" relurl=\"https://y.10086.cn/x/gah4\" style=\"margin-left:25px;\" class=\"btnBoxEs\">打开APP</div>",
+      "hreflink": "",
+      "expand": "9",
+      "weight": 100
+    }
+  ],
+  "web_414": [
+    {
+      "contentId": 60440,
+      "showrule": 2,
+      "positionid": 32212,
+      "content": "<li class=\"d-box\" logactionid=\"111863\" thingid=\"1\" jumppath=\"https://caiyun.feixin.10086.cn:7071/portal/caiyunOfficialAccount/index.html?path=earningTask&amp;sourceid=1168\"><div class=\"appImg\"><i class=\"garden-red \"></i> <img src=\"https://img.zone139.com/web_content_op/content/images/20230803/1691045141610.png\" style=\"height:24px;\" /></div><div class=\"b-flex\"><div class=\"d-box\"><h2 class=\"b-flex appTitle\">天天领红包</h2><p><br /></p></div><p><br /></p><p>用得越多，红包越多</p></div></li><li class=\"d-box\" logactionid=\"111863\" thingid=\"2\" jumppath=\"https://happy.mail.10086.cn/jsp/cn/gift/login/sso.do?sourceid=3003\"><div class=\"appImg\"><i class=\"garden-red \"></i> <img src=\"https://img.zone139.com/web_content_op/content/images/20230810/1691637382769.png\" /> </div><div class=\"b-flex\"><div class=\"d-box\"><h2 class=\"b-flex appTitle\">赢88元立减金</h2><p><br /></p></div><p><br /></p><p>登录139邮箱App，每月开红包</p></div></li><li class=\"d-box\" logactionid=\"111863\" thingid=\"3\" jumppath=\"https://dev.coc.10086.cn/coc/canvas/package-h5-canvas/online/mangopkg?pageId=1681988937445752832&amp;channelId=P00000057974\"><div class=\"appImg\"><i class=\"garden-red \"></i><img src=\"https://img.zone139.com/web_content_op/content/images/20230727/1690424134906.png\" /> </div><div class=\"b-flex\"><div class=\"d-box\"><h2 class=\"b-flex appTitle\">超值芒果包</h2><p><br /></p></div><p><br /></p><p>22元双视频会员+30GB超大流量</p></div></li>",
+      "expand": "9",
+      "weight": 100
+    }
+  ],
+  "web_431": [
+    {
+      "contentId": 60360,
+      "showrule": 1,
+      "positionid": 32675,
+      "content": "<div class=\"swiper-slide conBox\"><div class=\"slideItem\" relurl=\"toSelfUrl\" tourl=\"https://y.10086.cn/x/gah14\" action=\"111862\" thing=\"10\"><img src=\"/images/hcy/icon_wc.png\" alt=\"\" /> <div class=\"text_block\">微信备份</div></div><div class=\"slideItem\" relurl=\"toSelfUrl\" tourl=\"https://y.10086.cn/x/gah15\" action=\"111862\" thing=\"11\"><img src=\"/images/hcy/icon_xiangce.png\" alt=\"\" /> <div class=\"text_block\">相册备份</div></div><div class=\"slideItem\" relurl=\"collect\" action=\"111862\" thing=\"12\"><img src=\"/images/hcy/icon_wj.png\" alt=\"\" /> <div class=\"text_block\">文件收集</div></div><div class=\"slideItem\" relurl=\"toSelfUrl\" tourl=\"https://y.10086.cn/x/gah16\" action=\"111862\" thing=\"13\"><img src=\"/images/hcy/icon_pdf.png\" alt=\"\" /> <div class=\"text_block\">PDF工具</div></div></div>",
+      "hreflink": "",
+      "expand": "9",
+      "weight": 99
+    }
+  ],
+  "web_420": [
+    {
+      "contentId": 60441,
+      "showrule": 2,
+      "positionid": 32474,
+      "content": "<div class=\"d-box navBarList\"><div class=\"b-flex ta_av navBarList-item iconItems\" rel=\"toSelfUrl\" tourl=\"https://h.139.com/content/discoverNewVersion?columnId=20&amp;shareStatus=true&amp;channel=039\" actionid=\"109702\" thingid=\"2\"><div class=\"navBarList-itemBox\"><div class=\"conStyle\"><img width=\"100%\" src=\"https://img.zone139.com/web_content_op/content/images/20230816/1692154519965.png\" /> </div><p class=\"navBarList-itemtext\">追芒果综艺</p></div></div><div class=\"b-flex ta_av navBarList-item iconItems\" rel=\"toSelfUrl\" style=\"position:relative;\" tourl=\"https://caiyun.feixin.10086.cn:7071/portal/139newsignin/index.html?sourceid=1036\" actionid=\"111402\" thingid=\"11\"><div class=\"navBarList-itemBox\"><div class=\"conStyle\"><img width=\"100%\" src=\"https://img.zone139.com/web_content_op/content/images/20230803/1691052575127.png\" /> </div><p class=\"navBarList-itemtext\">签到领会员</p></div></div><div class=\"b-flex ta_av navBarList-item iconItems\" rel=\"downloadApp\" style=\"position:relative;\"><div class=\"navBarList-itemBox\"><div class=\"conStyle\"><img width=\"100%\" src=\"https://img.zone139.com/web_content_op/content/images/20230803/1691052637447.png\" /> </div><p class=\"navBarList-itemtext\">邮箱APP</p><img src=\"https://img.zone139.com/web_content_op/content/images/20230803/1691052666079.png\" style=\"position:absolute;height:12px;width:24px;right:9px;top:10px;\" /> </div></div><div class=\"b-flex ta_av navBarList-item iconItems\" rel=\"toSelfUrl\" style=\"position:relative;\" tourl=\"https://happy.mail.10086.cn/jsp/cn/gift/login/sso.do?sourceid=3012\" actionid=\"111402\" thingid=\"12\"><div class=\"navBarList-itemBox\"><div class=\"conStyle\"><img width=\"100%\" src=\"https://img.zone139.com/web_content_op/content/images/20230810/1691656240549.jpg\" /> </div><p class=\"navBarList-itemtext\">赢立减金</p></div></div></div>",
+      "expand": "9",
+      "weight": 100
+    }
+  ],
+  "web_425": [
+    {
+      "contentId": 60116,
+      "showrule": 1,
+      "positionid": 32574,
+      "content": "<div id=\"peLinkReadmail\" class=\"peLinkReadmail\"><section class=\"footBox\">        <div class=\"topFoot\"><span class=\"title\">打开方式</span> <span id=\"useBrowser\" class=\"sorft\">继续使用浏览器 <i class=\"iconfont i-next\"></i></span> </div><div class=\"appEmailBox\"><div class=\"appeMail\"><img src=\"/images/readmail/appEmail.png\" alt=\"\" class=\"appImg\" /> <div class=\"texts\">前往APP领立减金</div></div><button id=\"toAppReadMail\" class=\"btnStyle\">立即打开</button> </div></section></div>",
+      "hreflink": "",
+      "expand": "9",
+      "weight": 100
+    }
+  ],
+  "web_418": [
+    {
+      "contentId": 60013,
+      "showrule": 2,
+      "positionid": 32418,
+      "content": "<li data-hecaiyun-click=\"\" rel=\"SMS\" class=\"applicationitem\"><img data-hecaiyun-click=\"\" src=\"../images/home/office/icon06.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">发短信</p></li><li data-hecaiyun-click=\"\" rel=\"PAYCOST\" class=\"applicationitem\"><img data-hecaiyun-click=\"\" src=\"../images/home/move/icon01.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">查话费</p></li><li data-hecaiyun-click=\"\" rel=\"EINVOICETITLE\" class=\"applicationitem\"><img data-hecaiyun-click=\"\" src=\"../images/home/office/icon01.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">电子发票</p></li><li data-hecaiyun-click=\"\" rel=\"andjr\" class=\"applicationitem dHide\"><img data-hecaiyun-click=\"\" src=\"../images/home/move/icon03.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">and金融</p></li><li data-hecaiyun-click=\"\" rel=\"enterpriseServe\" class=\"applicationitem\"><img data-hecaiyun-click=\"\" src=\"../images/home/move/icon06.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">企业服务号</p></li>",
+      "expand": "9",
+      "weight": 94
+    }
+  ],
+  "web_424": [
+    {
+      "contentId": 60466,
+      "showrule": 1,
+      "positionid": 32539,
+      "content": "<p class=\"font-text\">更轻松管理账单和发票</p><p id=\"downloadAppMailAd\" iosurl=\"p139://y.10086.cn/x/gah17\" androidurl=\"p139://y.10086.cn/x/gah17\" activityurl=\"https://appdownload.mail.10086.cn/pe_tips/PE/download/_4022001.html\" class=\"btnBoxEs\">打开APP</p>",
+      "hreflink": "",
+      "expand": "9",
+      "weight": 100
+    }
+  ],
+  "web_416": [
+    {
+      "contentId": 60376,
+      "showrule": 2,
+      "positionid": 32416,
+      "content": "<li data-hecaiyun-click=\"\" rel=\"NOTE\" class=\"applicationitem\"><img data-hecaiyun-click=\"\" src=\"../images/home/office/icon02.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">云笔记</p></li><li data-hecaiyun-=\"\" click=\"\" rel=\"calendar_home\" class=\"applicationitem\" action=\"30127\" thing=\"23\"><img data-hecaiyun-click=\"\" src=\"../images/home/office/icon03.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">日历</p></li><li data-hecaiyun-click=\"\" rel=\"netdisk\" class=\"applicationitem\" action=\"104350\" thing=\"0\"><img data-hecaiyun-click=\"\" src=\"../images/home/office/icon10.png\" alt=\"\" /> <p data-hecaiyun-click=\"\"><span data-hecaiyun-click=\"\" style=\"position:relative;\"> 移动云盘 <span data-hecaiyun-click=\"\" id=\"yunpanRedDot\" class=\"redDot dHide\" style=\"position:absolute;top:0px;right:-6px;\"></span> </span> </p></li><li data-hecaiyun-click=\"\" rel=\"contacts\" class=\"applicationitem\" action=\"30127\" thing=\"5\"><img data-hecaiyun-click=\"\" src=\"../images/home/office/icon05.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">通讯录</p></li><li data-hecaiyun-click=\"\" rel=\"colle\" class=\"applicationitem\"><img data-hecaiyun-click=\"\" src=\"../images/home/office/colle.png\" alt=\"\" /> <p data-=\"\" hecaiyun-click=\"\">文件收集</p></li><li rel=\"flashMail\" class=\"applicationitem\"><img src=\"../images/home/office/flashMail.png\" alt=\"\" /> <p>发闪邮</p></li><li data-hecaiyun-click=\"\" rel=\"OFFICETEMPLATE\" class=\"applicationitem\"><img data-hecaiyun-click=\"\" src=\"../images/home/office/officeTemplate.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">办公模板</p></li><li data-hecaiyun-click=\"\" rel=\"PDFCHANGE\" class=\"applicationitem\"><img data-hecaiyun-click=\"\" src=\"../images/home/office/pdfChange.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">PDF转换</p></li><li data-hecaiyun-click=\"\" rel=\"MAILFILE\" class=\"applicationitem\" action=\"111862\" thing=\"24\"><img data-hecaiyun-click=\"\" src=\"../images/home/office/file.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">邮箱附件</p></li>",
+      "expand": "9",
+      "weight": 94
+    }
+  ],
+  "web_429": [
+    {
+      "contentId": 60142,
+      "showrule": 0,
+      "positionid": 32614,
+      "content": "<div id=\"peLinkmailList\" class=\"peLinkReadmail\"><section class=\"footBox\">        <div class=\"topFoot\"><span class=\"title\">打开方式</span> <span id=\"useBrowserMailList\" class=\"sorft\">继续使用浏览器 <i class=\"iconfont i-next\"></i></span> </div><div class=\"appEmailBox\"><div class=\"appeMail\"><img src=\"/images/readmail/appEmail.png\" alt=\"\" class=\"appImg\" /> <div class=\"texts\">前往APP领立减金</div></div><button id=\"toAppMailList\" relurl=\"https://y.10086.cn/x/gah5\" class=\"btnStyle\">立即打开</button> </div></section></div>",
+      "hreflink": "",
+      "expand": "9",
+      "weight": 100
+    }
+  ],
+  "web_423": [
+    {
+      "contentId": 60095,
+      "showrule": 1,
+      "positionid": 32538,
+      "content": "﻿<article class=\"layer_mask\" id=\"verificationBoxMask\" style=\"z-index:9998;\"></article><div class=\"tipsBox tipsBoxtThree\" id=\"verificationBox\" style=\"position:fixed;z-index:9999;border:none;right:8px;left:8px;bottom:1px;padding:0;height:127px;background:transparent;box-shadow:none;\"><img id=\"closeVerificationBoxBtn\" src=\"/images/close_ad.png\" style=\"position:absolute;right:10px;top:16px;width:20px;\" alt=\"\" /><img id=\"toDownload\" relurl=\"https://happy.mail.10086.cn/jsp/cn/gift/login/sso.do?sourceid=3005\" src=\"https://img.zone139.com/web_content_op/content/images/20230726/1690366716082.png\" style=\"width:100%;height:100%;object-fit:cover;\" alt=\"\" /> </div>",
+      "hreflink": "",
+      "expand": "9",
+      "weight": 100
+    }
+  ],
+  "web_417": [
+    {
+      "contentId": 60462,
+      "showrule": 2,
+      "positionid": 32417,
+      "content": "<li data-hecaiyun-click=\"\" rel=\"rtcenter\" class=\"applicationitem\"><img data-hecaiyun-click=\"\" src=\"../images/home/recom/icon01.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">权益中心</p></li><li data-hecaiyun-click=\"\" rel=\"toSelfUrl\" class=\"applicationitem\" tourl=\"https://caiyun.feixin.10086.cn:7071/portal/139newsignin/index.html?sourceid=1004\" style=\"position:relative;\" action=\"110409\" thing=\"4\"><img data-hecaiyun-click=\"\" src=\"../images/home/recom/icon06.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">139云朵中心</p><img src=\"https://img.zone139.com/web_content_op/content/images/20230316/1678949181564.png\" style=\"position:absolute;height:12px;width:24px;right:10%;top:-2px;color:#f00;font-size:22px;\" /> </li><li data-hecaiyun-click=\"\" rel=\"139Program\" class=\"applicationitem\"><img data-hecaiyun-click=\"\" src=\"../images/cloudImg/perCloud.png?v=s002\" alt=\"\" /> <p data-hecaiyun-click=\"\">云盘APP</p></li><li data-hecaiyun-click=\"\" id=\"statuSubscribeList\" rel=\"subscribeList\" class=\"applicationitem dHide\" action=\"30135\" thing=\"1\"><img data-hecaiyun-click=\"\" src=\"../images/home/recom/icon04.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">我的订阅</p></li><li data-hecaiyun-click=\"\" rel=\"139application\" class=\"applicationitem\" action=\"106282\" thing=\"2\" style=\"position:relative;\"><img data-hecaiyun-click=\"\" src=\"../images/home/recom/icon05.png\" alt=\"\" /> <p data-hecaiyun-click=\"\">邮箱APP</p><img src=\"https://img.zone139.com/web_content_op/content/images/20230316/1678949181564.png\" style=\"position:absolute;height:12px;width:24px;right:10%;top:-2px;color:#f00;font-size:22px;\" /> </li><li data-hecaiyun-click=\"\" class=\"applicationitem\"><a href=\"https://h.139.com/content/discoverNewVersion?columnId=20&shareStatus=true&channel=027\"><img data-hecaiyun-click=\"\" src=\"https://img.zone139.com/web_content_op/content/images/20230730/1690697095414.jpg\" alt=\"\" /> <p data-hecaiyun-click=\"\">发现广场</p></a></li>",
+      "expand": "9",
+      "weight": 98
+    }
+  ],
+  "web_422": [
+    {
+      "contentId": 60094,
+      "showrule": 1,
+      "positionid": 32536,
+      "content": "<article class=\"layer_mask\" id=\"verificationBoxMask\" style=\"z-index:9998;\"></article><div class=\"tipsBox tipsBoxtThree\" id=\"verificationBox\" style=\"position:fixed;z-index:9999;border:none;right:8px;left:8px;bottom:1px;padding:0;height:127px;background:transparent;box-shadow:none;\"><img id=\"closeVerificationBoxBtn\" src=\"/images/close_ad.png\" style=\"position:absolute;right:10px;top:16px;width:20px;\" alt=\"\" /><img id=\"toDownload\" relurl=\"https://happy.mail.10086.cn/jsp/cn/gift/login/sso.do?sourceid=3014\" src=\"https://img.zone139.com/web_content_op/content/images/20230726/1690366579792.png\" style=\"width:100%;height:100%;object-fit:cover;\" alt=\"\" /> </div>",
+      "hreflink": "",
+      "expand": "9",
+      "weight": 99
+    }
+  ]
+}
