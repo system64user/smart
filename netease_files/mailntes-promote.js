3d5c8d9b260401cbbbd95295af10fe9f2b1e9c87
system64user <144166914+system64user@users.noreply.github.com>
2023-09-05 20:36:30 -0700

    Add files via upload

---
+++ netease_files/mailntes-promote.js
@@ -0,0 +1,29 @@
+// 登录框下方广告配置
+var PopConfig = window.PopConfig = [
+    {
+        'name': '网易邮箱官方App', 
+        'className': 'mailApp-logo-default',
+        'link': 'https://mail.163.com/dashi/dlpro.html?from=mail92',
+        'tjName': 'b_login_box_logo-mailmaster_dl_click',
+        'remark': '立即下载',
+        'picUrl': 'https://onegoods.nosdn.127.net/resupload/2019/6/4/55dcb603c768222522d88de104b37a63.png'
+    },
+    {
+        'name': '升级VIP，安全性能提升30%', 
+        'className': 'mailApp-logo-default',
+        'link': 'https://reg1.vip.163.com/newReg1/?from=NeteaseLoginBoxLogoShow_02',
+        'tjName': 'b_login_box_logo-default_click',
+        'remark': '立即升级',
+        'picUrl': 'https://mimg.127.net/ggimg/all/img21/mail-index-bottom-gg-7.png'
+    }
+];
+
+// 左侧广告大图
+window.DefaultThemeMaterial = [{
+    type: 0,
+    bgColor: 'eff6ff',
+    bgCnt: 'https://mimg.127.net/p/freemail/index/lib/img/login-yeah-theme.jpg',
+    bInitLink: true,
+    userProperty: {},
+    bgLink: 'https://mail.163.com/client/dl.html?from=mail6'
+}];
